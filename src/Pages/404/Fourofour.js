4c0992f7577f03b7f0469416fc149973f22455c0
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2022-10-31 01:31:38 +0600

    404 page modified

--- src/Pages/404/Fourofour.js
+++ src/Pages/404/Fourofour.js
@@ -4,7 +4,8 @@ import { Container } from 'react-bootstrap';
 const Fourofour = () => {
     return (
         <div>
-            <Container className='w-25 m-auto'>
+            <Container className='w-25 mx-auto border p-5 m-5 shadow bg-dark text-white bg-opacity-50 rounded-4 text-center ' >
+                <h1 className='text-warning'>404</h1>
             <h2>Sorry The Page you are requesting is not available</h2>
             </Container>
         </div>
